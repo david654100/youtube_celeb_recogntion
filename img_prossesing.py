aac077e5477d38f00609b52d173d8c4defc0b71c
david <david654100@gmail.com>
2020-01-07 14:05:50 -0500

    finnished vid splitter and downloader

--- img_prossesing.py
+++ img_prossesing.py
@@ -2,38 +2,40 @@ import cv2
 import os
 
 
-
 def vid_splitter(dir_name: str):
     img = 'frames'
     if not os.path.exists(img):
         os.makedirs(img)
     directory = os.fsencode(dir_name)
+    rgb_frames = []
     count = 0
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
 
-
         if filename.endswith(".mp4"):
             full_path = os.path.abspath(os.path.join(directory, file))
             full_path = full_path.decode("utf-8")
-            full_path = full_path.replace("\\","\\\\")
+            full_path = full_path.replace("\\", "\\\\")
 
             print(full_path)
             vidcap = cv2.VideoCapture(full_path)
 
             success, image = vidcap.read()
 
-            rgb_frames = []
+            x=0
             while success:
-                vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 200))
+
+                vidcap.set(cv2.CAP_PROP_POS_MSEC, (x * 1000))
                 cv2.imwrite(os.path.join(img, "frame{:d}.jpg".format(count)), image)  # save frame as JPEG file
                 success, image = vidcap.read()
                 print('Read a new frame: ', success)
-                if not(image is None):
+                if not (image is None):
                     rgb_frames.append(image[:, :, ::-1])
 
                 count += 1
+                x +=1
 
     return rgb_frames
 
-vid_splitter("vids")
+
+

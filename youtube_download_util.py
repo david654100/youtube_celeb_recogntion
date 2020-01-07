61c240a0ba7a9d32d077dc7c4f13131aebade5de
david <david654100@gmail.com>
2020-01-06 21:31:43 -0500

    testing download

--- youtube_download_util.py
+++ youtube_download_util.py
@@ -1,16 +1,23 @@
-import pytube
+from pytube import YouTube
+from pytube import  Playlist
+import os
 
 
-def create_url(videoId: str) -> str:
-    return "https://www.youtube.com/watch?v=" + videoId
+def create_url(video_id: str) -> str:
+    return "https://www.youtube.com/watch?v=" + video_id
 
 
-def youtube_download(path: str, url: str):
-    youtube = pytube.YouTube(url)
-    video = youtube.streams.first()
-    video.download(path)
+def downloadYouTube(video_url:str, path:str):
+    yt = YouTube(video_url)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
 
-
-youtube_download("\\videos",create_url("uW1fIaxbT10"))
+def downloadPlaylist(playlist_url:str,path:str):
+    playlist = Playlist('https://www.youtube.com/watch?v=58PpYacL-VQ&list=UUd6MoB9NC6uYN2grvUNT-Zg')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    playlist.download_all(path)
 
 
